7c189069f1ab662b441a9a1ef214324c687b9c0c
karuana <emilykaruanamwangi@gmail.com>
2025-04-09 00:15:39 +0300

    Submit

--- src/components/App.js
+++ src/components/App.js
@@ -17,12 +17,21 @@ function Home() {
 }
 
 {/* write an <About> component here */}
-
+function About(){
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
+      {/* add the <About> component here */}
+      <About />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
